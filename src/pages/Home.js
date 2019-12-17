20d5bfeac31a9e222ba8e6ccf60331220ad0636b
tandemungwa <tmungwa@gmail.com>
2019-12-17 08:54:33 -0500

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -92,7 +92,7 @@ export default class Home extends PureComponent {
             style={{
               //width: "100%",
               //height: "100%",
-              opacity: 0.4,
+              opacity: 0.35,
               backgroundColor: "#424242"
             }}
             src={`https://image.tmdb.org/t/p/original/${media.backdrop_path}`}
